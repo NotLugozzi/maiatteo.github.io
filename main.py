ead3c4dff422ddffa1809c3c3430aa557508b3f2
Mercurio <47455213+NotLugozzi@users.noreply.github.com>
2023-01-07 17:52:21 +0100

    Pizzi works as intended, new table formatting
    Endless Goduria

--- main.py
+++ main.py
@@ -3,36 +3,54 @@ import json
 from jinja2 import Template
 map_id = "280304"
 response = requests.get(f'https://api.codetabs.com/v1/proxy?quest=https://scoresaber.com/api/leaderboard/by-id/{map_id}/scores?countries=it&page=1')
-pizzi = requests.get(f"https://api.codetabs.com/v1/proxy?quest=https://scoresaber.com/api/leaderboard/by-id/{map_id}/scores?countries=us&search=sionpizzi")
+pizzi = requests.get(f"https://scoresaber.com/api/leaderboard/by-id/{map_id}/scores?countries=us&search=sionpizzi")
 if response.status_code == 200:
     scores_dict = response.json()
-    h = str(pizzi)
-    newpizzi = h.replace("{'metadata': {'itemsPerPage': 12, 'page': 1, 'total': 1}, 'scores': [", "")
-    pizzi_json = newpizzi.json()
-    print(pizzi_json)
-    # print(scores_dict) godo
+    pizzi_dict = pizzi.json()
+    #print(pizzi_dict)
+    #print(scores_dict) # godo
     scores = scores_dict['scores'][:10]
+    pizziscore = pizzi_dict["scores"][:1]
+    scores.extend(pizziscore)
+    print(scores)
     with open('scores.json', 'w', encoding="utf-16") as f:
      json.dump(scores, f)
-    scores.append(pizzi_json)
-    scores.sort(key=lambda score: score['baseScore'], reverse=False)
+    print("json salvato")
+    scores.sort(key=lambda score: score['baseScore'], reverse=True)
     #placeholder
     template = Template("""
     <html>
     <head>
-        <title>zio pera</title>
+        <title>BSEUC Qualifiers Italia</title>
+        <style>
+           table, th, td {
+           border: 2px solid black;
+           text-align: center;
+           }
+           h1, h2, h3{
+           text-align: center;
+           }
+           .center {
+            margin-left: auto;
+            margin-right: auto;
+            }
+        </style>
     </head>
     <body>
-        <h1>punteggio del godo</h1>
-        <table>
+        <h3>placeholder map name</h3>
+        <table class="center">
         <tr>
-            <th>Player</th>
-            <th>Score</th>
+            <th style="padding:10px">Player</th>
+            <th style="padding:10px">Score</th>
+            <th style="padding:10px">FC</th>
+            <th style="padding:10px">Last Played Date</th>
         </tr>
         {% for score in scores %}
            <tr>
             <td>{{ score.leaderboardPlayerInfo.name }}</td>
             <td>{{ score.baseScore }}</td>
+            <td>{{ score.fullCombo }}</td>
+            <td>{{ score.timeSet }}</td>
           </tr>
           {% endfor %}
         </table>
@@ -42,7 +60,5 @@ if response.status_code == 200:
     html = template.render(scores=scores)
     with open("index.html", "w", encoding="utf-16") as f:
      f.write(html)
-    with open('scores.json', 'w') as f:
-     json.dump(scores, f)    
 else:
     print("An error occurred:", response.status_code)
